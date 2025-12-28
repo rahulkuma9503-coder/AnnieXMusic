511e6db5c33061ecd2631d1ec62bd51b7239789c
rahulkuma9503-coder <rahulkuma9503@gmail.com>
2025-12-28 21:05:47 +0530

    rahul

--- ANNIEMUSIC/platforms/Youtube.py
+++ ANNIEMUSIC/platforms/Youtube.py
@@ -6,7 +6,7 @@ from typing import Dict, List, Optional, Tuple, Union
 import yt_dlp
 from pyrogram.enums import MessageEntityType
 from pyrogram.types import Message
-from youtubesearchpython.__future__ import VideosSearch
+from youtubesearchpython.aio import VideosSearch
 
 from ANNIEMUSIC.utils.database import is_on_off
 from ANNIEMUSIC.utils.downloader import yt_dlp_download, download_audio_concurrent
--- ANNIEMUSIC/plugins/bot/inline.py
+++ ANNIEMUSIC/plugins/bot/inline.py
@@ -3,7 +3,7 @@ from pyrogram.types import (
     InlineKeyboardMarkup,
     InlineQueryResultPhoto,
 )
-from youtubesearchpython.__future__ import VideosSearch
+from youtubesearchpython.aio import VideosSearch
 
 from ANNIEMUSIC.utils.inlinequery import answer
 from config import BANNED_USERS
